f4cac7520f486d5e242078c51f75e2ac90d3ee06
Shireesh Kantharaj <52453070+SBKGit@users.noreply.github.com>
2023-08-30 12:23:22 +0100

    Update main.tf

--- VPC_Datasync/main.tf
+++ VPC_Datasync/main.tf
@@ -79,7 +79,7 @@ resource "aws_instance" "datasync_instance" {
   subnet_id            = data.terraform_remote_state.vpc_1.outputs.private_subnet
   security_groups      = [aws_security_group.datasync_security_group.id]
   iam_instance_profile = aws_iam_instance_profile.datasync_profile.name
-  key_name             = "Ec2_keypair"
+  key_name             = "datasyncdev"
   user_data            = <<-EOF
               #!/bin/bash
               yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
