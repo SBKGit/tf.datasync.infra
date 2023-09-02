f439745dda1899fbaa3b9de9e184c80cff6fa502
Shireesh Kantharaj <52453070+SBKGit@users.noreply.github.com>
2023-09-02 22:50:17 +0100

    Update backend.tf

--- vpc_datasync/backend.tf
+++ vpc_datasync/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-datatf" #updated with bucket name
+    bucket  = "datasynctfgmdev" #updated with bucket name
     key     = "vpc_datasync/vpc_datasync.tfstate"
     encrypt = true
     region  = "eu-west-2"
