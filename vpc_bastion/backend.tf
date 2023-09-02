ae5d3944ab5045ed221cc881ca42756ad7c565a4
Shireesh Kantharaj <52453070+SBKGit@users.noreply.github.com>
2023-09-02 22:49:57 +0100

    Update backend.tf

--- vpc_bastion/backend.tf
+++ vpc_bastion/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-datatf" #updated with bucket name
+    bucket  = "datasynctfgmdev" #updated with bucket name
     key     = "vpc_bastion/vpc_bastion.tfstate"
     encrypt = true
     region  = "eu-west-2"
