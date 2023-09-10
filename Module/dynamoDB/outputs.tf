7f2e7628a4f83bb4601a2e7e8ac50c304ca0606c
Prashanth Palanisamy <palanisamy.prashanth92@gmail.com>
2023-09-10 20:17:57 +0100

    updated

--- Module/dynamoDB/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "dynamoDB_id" {
-  value       = aws_dynamodb_table.dynamoDB.id
-  description = "output to display role id"
-}
-
-output "dynamoDB_arn" {
-  value       = aws_dynamodb_table.dynamoDB.arn
-  description = "output to display role id"
-}
--- Module/subnet/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "subnet_id" {
-  value       = aws_subnet.subnet.id
-  description = "output to display subnet id"
-}
-
-output "subnet_arn" {
-  value       = aws_subnet.subnet.arn
-  description = "output to display subnet arn"
-}
