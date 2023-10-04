5b3e4c15ccc505088c7fb80806778d7839115bd9
Prashanth Palanisamy <119257910+prashanth-palanisamy@users.noreply.github.com>
2023-10-04 16:52:35 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 for ((dir=2; dir <= $#; dir++));do
-          echo "Deploying Terraform configurations in $dir"
-          cd "$dir"
+          echo "Deploying Terraform configurations in ${!dir}"
+          cd "${!dir}"
           echo "terraform init -backend-config=$1/backend.tfvars"
           terraform init -backend-config=$1/backend.tfvars
           terraform validate
