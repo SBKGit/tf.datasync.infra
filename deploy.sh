d83dfff77329ae3021f5cf0524c8288c98e64b5b
Prashanth Palanisamy <119257910+prashanth-palanisamy@users.noreply.github.com>
2023-10-04 16:17:55 +0100

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+for dir in {$2..$END}; do
+          echo "Deploying Terraform configurations in $dir"
+          cd "$dir"
+          echo "terraform init -backend-config=$1/backend.tfvars"
+          terraform init -backend-config=$1/backend.tfvars
+          terraform validate
+          echo "terraform plan -var-file=$1/$1.tfvars -out=tfplan"
+          terraform plan -var-file=$1/$1.tfvars -out=tfplan
+          cd -
+          done
